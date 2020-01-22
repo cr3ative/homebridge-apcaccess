d2ef7f33428b5d7a239ebc10686ce8b669ebe37b
Paul Curry <paulcurry@paulsmbpretina.lan>
2020-01-22 14:25:53 +0000

    huarrgh

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # homebridge-apcaccess
 
-An extremely dumb [apcaccess](https://github.com/mapero/apcaccess) wrapper for [Homebridge](https://github.com/nfarina/homebridge).
+A moderately dumb [apcaccess](https://github.com/mapero/apcaccess) wrapper for [Homebridge](https://github.com/nfarina/homebridge).
 
-Supports precisely one UPS and only reports if it's being used (i.e, mains power has gone away) or not.
+Supports precisely one UPS, appears as a Contact Sensor, and reports:
 
-Large chunks of the actual clever bit of this plugin are from [homebridge-accessory-apcupsd](https://github.com/homespun/homebridge-accessory-apcupsd). Many thanks to homespun.
+* Battery Level
+* Low Battery State
+* Charging State
+* If we're on mains power or not
+
+This is essentially a worse version of [homebridge-accessory-apcupsd](https://github.com/homespun/homebridge-accessory-apcupsd) which I was too dumb to get working; many thanks to homespun.
 
 # Configuration
 
@@ -12,7 +17,7 @@ Whack something like this in your `accessories: []` section of homebridge config
 
 ```
 {
-    "name": "APC Access",
+    "name": "UPS",
     "accessory": "APCAccess",
     "host": "192.168.86.34",
     "port": 3551,
--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 "use strict";
 
+// {
+//   APC: '001,027,0658',
+//   DATE: '2020-01-22 13:49:38 +0000  ',
+//   HOSTNAME: 'pi.local',
+//   VERSION: '3.14.14 (31 May 2016) debian',
+//   UPSNAME: 'SMT750i',
+//   CABLE: 'USB Cable',
+//   DRIVER: 'USB UPS Driver',
+//   UPSMODE: 'Stand Alone',
+//   STARTTIME: '2020-01-21 22:41:56 +0000  ',
+//   MODEL: 'Smart-UPS 750 ',
+//   STATUS: 'ONLINE ',
+//   BCHARGE: '100.0 Percent',
+//   TIMELEFT: '270.0 Minutes',
+//   MBATTCHG: '5 Percent',
+//   MINTIMEL: '3 Minutes',
+//   MAXTIME: '0 Seconds',
+//   ALARMDEL: '30 Seconds',
+//   BATTV: '27.1 Volts',
+//   NUMXFERS: '0',
+//   TONBATT: '0 Seconds',
+//   CUMONBATT: '0 Seconds',
+//   XOFFBATT: 'N/A',
+//   STATFLAG: '0x05000008',
+//   MANDATE: '2013-02-02',
+//   SERIALNO: 'AS1305112528  ',
+//   NOMBATTV: '24.0 Volts',
+//   FIRMWARE: 'UPS 08.3 / ID=18',
+//   'END APC': '2020-01-22 13:49:57 +0000  '
+// }
+
 let Service, Characteristic;
 var ApcAccess = require("apcaccess");
 
 module.exports = homebridge => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory(
-    "homebridge-apcaccess",
-    "APCAccess",
-    ContactSensor
-  );
+  homebridge.registerAccessory("homebridge-apcaccess", "APCAccess", APCAccess);
 };
 
-function ContactSensor(log, config) {
+function APCAccess(log, config) {
   this.log = log;
   this.host = config["host"] || "localhost";
   this.port = config["port"] || "3551";
@@ -29,7 +56,7 @@ function ContactSensor(log, config) {
     .connect(this.host, this.port)
     .then(() => {
       this.log("Connected!");
-      this.queryAPCAccess.bind(this);
+      this.getContactState.bind(this);
     })
     .catch(err => {
       this.log("Couldn't connect to service:", err);
@@ -45,25 +72,76 @@ function ContactSensor(log, config) {
 
   this.contactSensor
     .getCharacteristic(Characteristic.ContactSensorState)
-    .on("get", this.queryAPCAccess.bind(this));
+    .on("get", this.getContactState.bind(this));
+  this.contactSensor
+    .getCharacteristic(Characteristic.BatteryLevel)
+    .on("get", this.getBatteryLevel.bind(this));
+  this.contactSensor
+    .getCharacteristic(Characteristic.ChargingState)
+    .on("get", this.getChargingState.bind(this));
+  this.contactSensor
+    .getCharacteristic(Characteristic.StatusLowBattery)
+    .on("get", this.getStatusLowBattery.bind(this));
 }
 
-ContactSensor.prototype = {
-  queryAPCAccess: function(callback) {
+APCAccess.prototype = {
+  getBatteryLevel: function(callback) {
     this.client.getStatusJson().then(result => {
-      let flags = result.STATFLAG;
-      let value = [flags & 0x08 ? "CONTACT_DETECTED" : "CONTACT_NOT_DETECTED"];
+      // BCHARGE
+      let percentage = parseInt(result.BCHARGE, 10);
+      this.log("Battery Level: ", percentage);
+      this.contactSensor.setCharacteristic(
+        Characteristic.BatteryLevel,
+        percentage
+      );
+      callback(null, percentage);
+    });
+  },
+  getChargingState: function(callback) {
+    this.client.getStatusJson().then(result => {
+      // STATFLAG
+      let percentage = parseInt(result.BCHARGE, 10);
+      let value =
+        result.STATFLAG & 0x80
+          ? "NOT_CHARGEABLE"
+          : result.STATFLAG & 0x10 || percentage === 100
+          ? "NOT_CHARGING"
+          : "CHARGING";
+      this.log("Charging state: ", value);
+      this.contactSensor.setCharacteristic(Characteristic.ChargingState, value);
+      callback(null, Characteristic.ChargingState[value]);
+    });
+  },
+  getStatusLowBattery: function(callback) {
+    this.client.getStatusJson().then(result => {
+      // STATFLAG
+      let value =
+        result.STATFLAG & 0x40 ? "BATTERY_LEVEL_LOW" : "BATTERY_LEVEL_NORMAL";
+      this.log("Low Battery? ", value);
+      this.contactSensor.setCharacteristic(
+        Characteristic.StatusLowBattery,
+        value
+      );
+      callback(null, Characteristic.StatusLowBattery[value]);
+    });
+  },
+  getContactState: function(callback) {
+    this.client.getStatusJson().then(result => {
+      let value = [
+        result.STATFLAG & 0x08 ? "CONTACT_DETECTED" : "CONTACT_NOT_DETECTED"
+      ];
       this.contactSensor.setCharacteristic(
         Characteristic.ContactSensorState,
         value
       );
       if (callback) {
-        callback(null, value);
+        callback(null, Characteristic.ContactSensorState[value]);
       }
     });
   },
   getServices: function() {
-    setInterval(this.queryAPCAccess.bind(this), this.interval * 1000);
+    // watch contact state on an interval
+    setInterval(this.getContactState.bind(this), this.interval * 1000);
     return [this.informationService, this.contactSensor];
   }
 };
